6713a8b0a83b9c841ecbef5eff4ffb760e19880e
leesg17 <leesg0107@gmail.com>
2024-04-28 16:50:50 +0900

    [ADD] writing message func

--- letterboat/src/pages/app.rs
+++ letterboat/src/pages/app.rs
@@ -1,21 +1,16 @@
-use crate::pages::{home::HomePage, not_found::NotFound};
+use crate::pages::{home::HomePage, not_found::NotFound, write_message::WriteMessage};
 use leptos::*;
-use leptos_meta::*;
 use leptos_router::*;
 
 #[component]
 pub fn App() -> impl IntoView {
-    provide_meta_context();
-
     view! {
-        <Stylesheet id="leptos" href="/pkg/letterboat.css"/>
-        <Title text="Welcome to Leptos"/>
-
         <Router>
             <main>
                 <Routes>
-                    <Route path="" view=HomePage/>
-                    <Route path="/*any" view=NotFound/>
+                    <Route path="" view=|| view! { <HomePage/> }/>
+                    <Route path="/write" view=|| view! { <WriteMessage/> }/>
+                    <Route path="*" view=|| view! { <NotFound/> }/>
                 </Routes>
             </main>
         </Router>
--- letterboat/src/pages/home.rs
+++ letterboat/src/pages/home.rs
@@ -1,10 +1,15 @@
-use crate::components::Counter;
 use leptos::*;
+use leptos_router::*;
 
 #[component]
 pub fn HomePage() -> impl IntoView {
     view! {
-        <h1>"Welcome to Leptos11111!"</h1>
-        <Counter />
+        <div class="flex flex-col items-center justify-center h-screen">
+            <h1 class="text-4xl font-bold mb-4">"Welcome to Leptos!"</h1>
+            <p class="text-xl mb-8">"This is the home page."</p>
+            <a href="/write" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+                "Go to Write Message"
+            </a>
+        </div>
     }
 }
