695da571126db4a14baefc1f3880b5252128beaa
pbit <alstjqrhkr2018@gmail.com>
2024-05-12 10:05:25 +0900

    update

--- letterboat/src/pages/home.rs
+++ letterboat/src/pages/home.rs
@@ -7,9 +7,16 @@ pub fn HomePage() -> impl IntoView {
         <div class="flex flex-col items-center justify-center h-screen">
             <h1 class="text-4xl font-bold mb-4">"Welcome to Leptos!"</h1>
             <p class="text-xl mb-8">"This is the home page."</p>
-            <a href="/write" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+>            <a href="/write" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
                 "Go to Write Message"
             </a>
+          <hr/> 
+            <a href="/letterview" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+                "Go to letter viewe"
+            </a>
+
+
+
         </div>
     }
 }
--- letterboat/src/pages/letter_view.rs
+++ letterboat/src/pages/letter_view.rs
@@ -3,6 +3,11 @@ use leptos::*;
 #[component]
 pub fn LetterView() -> impl IntoView {
     view! {
-            <h1>"메시지 보기"</h1>
+        <div class="max-w-2xl mx-auto p-6 bg-white shadow-md rounded-lg">
+            <h1 class="text-2xl font-bold mb-4">"메시지 제목"</h1>
+            <div class="border-t border-gray-200 mt-4">
+                메시지 컨텐트
+            </div>
+        </div>
     }
 }
